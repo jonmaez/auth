fb74cd70bdc9d20638ef3ef554167a9889487a10
Ahsan Bhai <ahsan_bhai@trendmicro.com>
2020-05-19 11:45:36 -0500

    Fixed stuff

--- src/main/java/com/ahsanb/auth/util/JwtUtils.java
+++ src/main/java/com/ahsanb/auth/util/JwtUtils.java
@@ -1,9 +1,11 @@
 package com.ahsanb.auth.util;
 
 import java.util.Date;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import com.ahsanb.auth.security.services.UserDetailsImpl;
@@ -13,20 +15,24 @@ import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
 public class JwtUtils {
-	@Value("${bezkoder.app.jwtSecret}")
-	private String jwtSecret;
+    @Value("${security.jwt.expiration:#{24*60*60}}")
+    private int expiration;
 
-	@Value("${bezkoder.app.jwtExpirationMs}")
-	private int jwtExpirationMs;
+    @Value("${security.jwt.secret:JwtSecretKey}")
+    private String secret;
 
 	public String generateJwtToken(Authentication authentication) {
 		UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
 
 		return Jwts.builder()
 				.setSubject((userPrincipal.getUsername()))
+				// Convert to list of strings. 
+				// This is important because it affects the way we get them back in the Gateway.
+				.claim("authorities", authentication.getAuthorities().stream()
+					.map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
 				.setIssuedAt(new Date())
-				.setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
-				.signWith(SignatureAlgorithm.HS512, jwtSecret)
+				.setExpiration(new Date((new Date()).getTime() + expiration * 1000))
+				.signWith(SignatureAlgorithm.HS512, secret)
 				.compact();
 	}
 }
