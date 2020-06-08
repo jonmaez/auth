069f19cac8e78cf2e80639b64e8d9ec5292f606a
Ahsan Bhai <ahsan_bhai@trendmicro.com>
2020-06-08 14:21:38 -0500

    Added multitenancy classes

--- src/main/java/com/ahsanb/auth/AuthApplication.java
+++ src/main/java/com/ahsanb/auth/AuthApplication.java
@@ -2,10 +2,11 @@ package com.ahsanb.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class AuthApplication {
 
 	public static void main(String[] args) {
